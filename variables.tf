cc915ca3cf7efb25e9e461db904584dad19672e5
ahmed04762 <owais.helix04762@gmail.com>
2023-06-28 22:03:30 -0400

    terraform modules added

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "accesskey" {
+	default="" 
+}
+
+variable "secretkey" {
+	default=""
+}
+
+variable "vpcID" {
+	default="vpc-05075705a4f808592"
+}
+variable "amiID" {
+	default="ami-02b8534ff4b424939"
+}
+variable "keyPair" {
+	default="My_EC2_Key"
+}
+variable "keyPair1" {
+	default="My_EC2_Key.pem"
+}
+
+
+
+
